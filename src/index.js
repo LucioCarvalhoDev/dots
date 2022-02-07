1f011600b70fbb2bbe9702956a7bc28ab3b5091e
Lucio <luciocarvalhodev@gmail.com>
2022-02-07 13:54:09 -0300

    controls population input

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import Controller from "./js/Controller.js";
 
 export const GAME_RULES = {
     dotDistanceToDie: 200,
-    dotInitialPopularion: 20,
+    dotPopulation: 20,
     lineMaxLenght: 200
 
 };
 
+// config ui controls
 const controls = document.querySelector('.ui_cntrls');
 document.querySelector('.ui_sensor_open').onclick = () => {
-    controls.classList.toggle('off')
-}
+    controls.classList.toggle('off');
+};
 
+// update GAME_RULES
+const iptPopulation = document.getElementById('ipt-population');
+iptPopulation.value = GAME_RULES.dotPopulation;
+iptPopulation.onchange = e => {
+    GAME_RULES.dotPopulation = e.target.value;
+};
+
+// play and pause
 let flagPlay = true;
 document.querySelector('.play').onclick = () => {
     flagPlay = !flagPlay;
@@ -19,7 +28,7 @@ document.querySelector('.play').onclick = () => {
 
 const controller = new Controller(document.getElementById('canvas'));
 const screen = document.querySelector('body');
-controller.populate(GAME_RULES.dotInitialPopularion);
+controller.populate(GAME_RULES.dotPopulation);
 controller.renderDots();
 controller.renderLines();
 
@@ -27,9 +36,8 @@ function gameLoop() {
     controller.display(screen.clientWidth, screen.clientHeight);
     if (flagPlay) {
         const population = controller.updateDots();
-
-        if (population < GAME_RULES.dotInitialPopularion)
-            controller.rePopulate(GAME_RULES.dotInitialPopularion - population);
+        if (population < GAME_RULES.dotPopulation)
+            controller.rePopulate(GAME_RULES.dotPopulation - population);
     }
     controller.renderDots();
     controller.renderLines();
